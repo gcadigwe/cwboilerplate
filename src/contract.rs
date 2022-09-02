77a1e4a99b18952089c5c06d4f7d89f723ebbce1
Godswill Adigwe <gcadigwe@gmail.com>
2022-09-02 21:41:54 +0100

    initial commit

---
+++ src/contract.rs
@@ -0,0 +1,43 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Config,CONFIG};
+use cosmwasm_std::Addr;
+
+
+// const CONTRACT_NAME: &str = "crates.io:whitelist";
+// const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    mut msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ src/state.rs
@@ -0,0 +1,14 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::Addr;
+use cw_storage_plus::{Item,Map};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub admin: Addr,
+}
+
+
+
+pub const CONFIG: Item<Config> = Item::new("config");
